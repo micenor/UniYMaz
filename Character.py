a7bf47466859ebc49592a361e6a6c0e0e34a2ba7
Miguel <micenor88@gmail.com>
2020-11-09 00:42:55 +0100

    Habilidades y ronda en main

--- Character.py
+++ Character.py
@@ -1,3 +1,5 @@
+from random import choice, randrange
+import math
 class Character():
     class Bookworn():
         life = 25
@@ -20,6 +22,7 @@ class Character():
                             if (1 <= choice<= len(dead)):
                                 chosen = dead[choice-1]
                                 chosen.life = chosen.__class__.life
+                                self.cooldown = 4
                                 break
                             else:
                                 print("Incorrect choice. Choice must be between 1 and ",len(dead),".",end=" ")
@@ -41,27 +44,77 @@ class Character():
         damage = 10
         player = 0
         name = "Worker"
-        """implementar habilidad"""
+        cooldown = 0
+        def ability(self,game):
+            if (self.cooldown==0):
+                enemy_target = choice(game.stage_enemies)
+                damage = 1.5 * (self.damage + randrange(self.damage + 1))
+                enemy_target.life = enemy_target.life - damage
+                print(("%s (Player %i) did %i damage to %s. %s has %iHP left. ") % (self.name, self.player, damage, enemy_target.name, enemy_target.name, enemy_target.life))
+                if (enemy_target.life <= 0):
+                    print("The enemy has died.")
+                self.cooldown = 3
+            else:
+                print("The skill is currently in cooldown for %s more rounds." % (self.cooldown))
+                return False
         def printInfo(self):
             print("The worker -> Stats: 40HP and 10DMG")
             print("         Skill: 1.5 * (DMG + DMG roll) damage to one enemy (3 rounds)")
+
 
     class Whatsapper():
         life = 20
         damage = 6
         player = 0
         name = "Whatsapper"
-        """implementar Habilidad"""
+        cooldown = 0
+        def ability(self,game):
+            if (self.cooldown==0):
+                alive = [character for character in game.characters_list if character.life > 0]
+                while True:
+                    i = 1
+                    for character in alive :
+                        print(i, ".-", end=" ")
+                        character.printInfo()
+                        i += 1
+                    try:
+                        choice = int(input("Who do you want to heal?: "))
+                        if (1 <= choice <= len(alive)):
+                            chosen = alive[choice - 1]
+                            chosen.life = 2*self.damage
+                            self.cooldown = 3
+                            break
+                        else:
+                            print("Incorrect choice. Choice must be between 1 and ", len(alive), ".", end=" ")
+                    except:
+                        print("Error, you must enter a number")
+            else:
+                print("The skill is currently in cooldown for %s more rounds." % (self.cooldown))
+                return False
         def printInfo(self):
             print("The whatsapper -> Stats: 20HP and 6DMG")
             print("         Skill: Heals 2*DMG to one player (3 rounds)")
+
 
     class Procrastinator():
         life = 30
         damage = 6
         player = 0
         name = "Procrastinator"
-        """implementar pasiva"""
+        cooldown = 0
+        def ability(self,game):
+            if ((self.cooldown==0) and (game.round >=3)):
+                enemy_target = choice(game.stage_enemies)
+                damage = self.damage + randrange(self.damage + 1) + game.round
+                enemy_target.life = enemy_target.life - damage
+                print(("%s (Player %i) did %i damage to %s. %s has %iHP left. ") % (self.name, self.player, damage, enemy_target.name, enemy_target.name, enemy_target.life))
+                if (enemy_target.life <= 0):
+                    print("The enemy has died.")
+                self.cooldown = 3
+            else:
+                print("The skill is currently in cooldown for %s more rounds." % (self.cooldown))
+                return False
+
         def printInfo(self):
             print("The procrastinator-> Stats: 30HP and 6DMG")
             print("         Passive: Adds +1 DMG each round. Resets at the beginning of each level.")
--- Game.py
+++ Game.py
@@ -1,14 +1,17 @@
-import Enemy,time,sys,Character,math
+import Enemy,sys,math
 from random import choice,randrange
 class Game():
     characters_list=[]
+    stage_enemies = []
+    round
     def executeStage(self, numStages,characters):
+        round = 0
         available_enemies = [Enemy.Enemy.Partial_Exam, Enemy.Enemy.Theoretical_Class, Enemy.Enemy.Teacher]
         alive_characters = characters
         self.characters_list = characters
         for i in (range(1,numStages+1)):
+            self.stage_enemies.clear()
             if (i == 4): available_enemies.append(Enemy.Enemy.Final_Exam)
-            stage_enemies = []
             print("         ************************")
             print(("         *       STAGE %i       *") % i)
             print("         ************************")
@@ -17,15 +20,16 @@ class Game():
             print("    ++++++++++++++++++++++++++++++++++++++")
             for n in range(4):
                 enemy = choice(available_enemies)
-                stage_enemies.append(enemy())
+                self.stage_enemies.append(enemy())
                 print(("    %s: Stats: %iHP and %iDMG") % (enemy.name, enemy.life, enemy.damage))
             print("    ++++++++++++++++++++++++++++++++++++++")
-            while((len(stage_enemies)!=0) and (len(alive_characters)!=0)):
+            while((len(self.stage_enemies)!=0) and (len(alive_characters)!=0)):
+                self.round += 1
                 print("    ------------------------")
                 print("    -    PLAYERS TURN      -")
                 print("    ------------------------")
                 for character in alive_characters:
-                    if(len(stage_enemies)!=0):
+                    if(len(self.stage_enemies)!=0):
                         while True:
                             print(("%s (Player %i). What are you going to do? ((A)ttack/(S)kill)") % (character.name,character.player))
                             option = input().upper()
@@ -35,19 +39,19 @@ class Game():
                                 if character.ability(self):
                                     break
                             else: break
-                        enemy_target = choice(stage_enemies)
+                        enemy_target = choice(self.stage_enemies)
                         if (option=="A"):
                             damage = randrange(character.damage + 1)
                             enemy_target.life = enemy_target.life - damage
                             print(("%s (Player %i) did %i damage to %s. %s has %iHP left. ") % (character.name, character.player,damage,enemy_target.name,enemy_target.name,enemy_target.life))
                             if (enemy_target.life<=0):
-                                stage_enemies.remove(enemy_target)
+                                self.stage_enemies.remove(enemy_target)
                                 print("The enemy has died.")
-                if((len(stage_enemies)!=0)):
+                if((len(self.stage_enemies)!=0)):
                     print("    ------------------------")
                     print("    -    MONSTERS TURN     -")
                     print("    ------------------------")
-                    for enemy in stage_enemies:
+                    for enemy in self.stage_enemies:
                         if (len(alive_characters)!=0):
                             character_target = choice(alive_characters)
                             option = choice(["A"])
@@ -66,7 +70,7 @@ class Game():
                     character.life = character_class.life
                 else:
                     character.life = math.ceil(character.life + character_class.life / 4)
-            if(len(stage_enemies)==0): print("Stage clear, all enemies defeated")
+            if(len(self.stage_enemies)==0): print("Stage clear, all enemies defeated")
             else:
                 print("All characters have been defeated. Try again.")
                 sys.exit(2)
